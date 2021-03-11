78a5352fd75cb23dd8e56f6b28ded185c87a085a
Volodymyr <vova.miretskyi@gmail.com>
2021-03-11 11:42:27 +0200

    you should add second layout bg

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
 import Header from './components/secondLesson/header';
 import Layout from './components/secondLesson/layout';
 import Footer from './components/secondLesson/footer';
+import bg1 from './components/secondLesson/img/bg1.jpg';
+import bg2 from './components/secondLesson/img/bg2.jpg';
 
 const App = () => {
+  const headerTitle = "Hello";
+  const headerDescr = 'React.js!';  
   const layoutTitle = "Hello React!";
   const layoutDesc = "Learning React is cool!";
+  const bgcForSecondLayout ='red';
   return(
     <>  
-        <Header />
-        <Layout 
-        title = {layoutTitle}
-        desc = {layoutDesc}
+        <Header 
+        title = {headerTitle}
+        descr = {headerDescr}
         />
         <Layout 
         title = {layoutTitle}
-        desc = {layoutDesc}
+        descr = {layoutDesc}
+        urlBg = {bg1}
+        />
+        <Layout 
+        title = {layoutTitle}
+        descr = {layoutDesc}
+        colorBg = {bgcForSecondLayout}
         />
         <Layout 
           title = {layoutTitle}
-          desc = {layoutDesc}
+          descr = {layoutDesc}
+          urlBg = {bg2}
         />
-        <Layout
-         title = {layoutTitle}
-         desc = {layoutDesc}
-         />
         <Footer />
     </>
   );
--- src/components/secondLesson/layout.js
+++ src/components/secondLesson/layout.js
@@ -1,20 +1,23 @@
 import l from './layout.module.css'
 
-const Layout = ({title, desc}) =>{
+const Layout = ({title, desc, urlBg, colorBg}) =>{
+    const lBackground = {urlBg} ? {background : `url(${urlBg})`} : {background : {colorBg}};
     return(
-    <section class={l.root}>
-        <div class="wrapper">
-            <article>
-                <div class={l.title}>
-                    <h3>{title}</h3>
-                    <span class={l.separator}></span>
-                </div>
-                <div class={l.desc, l.full}>
-                    <p>{desc}</p>
-                </div>
-            </article>
-        </div>
-    </section>
+    <>
+        <section class={l.root} style = {lBackground}>
+            <div class="wrapper">
+                <article>
+                    <div class={l.title}>
+                        <h3>{title}</h3>
+                        <span class={l.separator}></span>
+                    </div>
+                    <div class={l.desc, l.full}>
+                        <p>{desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </>
     );
 }
 
